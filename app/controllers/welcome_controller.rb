9ce1fddcf3215f365244a38fb5168ce1ddcaf073
Chris Romero <chrisleeromero1@gmail.com>
2022-05-26 23:29:22 -0600

    feat: add faraday and use figaro for x api key, begin work on parks controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+    connection = Faraday.new(url: "https://developer.nps.gov") do |faraday|
+      faraday.headers["X-API-KEY"] = ENV['national_parks_api_key']
+    end
+
+    response = connection.get("/api/v1/parks")
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
